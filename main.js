648f036f0cbefeb106d675c5d25e116a100fcfb5
gfCrova <giancrova.cl5@gmail.com>
2022-11-06 23:10:58 -0300

    resolviendo parentElement

--- main.js
+++ main.js
@@ -8,22 +8,44 @@ class Producto {
         this.fecha = fecha
         this.cantidad = cantidad
     }
-
 }
 
 class Interfaz {
-    agregarProducto(){
-
+    agregarProducto(product){
+        const listaProductos = document.getElementById('lista-productos');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card bg-light cardPadre text-center m-4 rounded-5">
+                <div class="card-body">
+                    <ul class="d-flex justify-content-around align-items-center list-unstyled f-5">
+                        <li><strong>Product</strong>: ${product.nombre}</li>
+                        <li><strong>Precio</strong>: ${product.precio}</li>
+                        <li><strong>Fecha</strong>: ${product.fecha}</li>
+                        <li><strong>Cantidad</strong>: ${product.cantidad}</li>
+                        <a href="#" class="btn btn-primary" name="delete">Delete</a>
+                    </ul>
+                </div>
+            </div>
+        `;
+        listaProductos.appendChild(element);
     }
 
-    eliminarProducto(){
+    resetForm(){
+        document.getElementById('formProductos').reset();
+    }
 
+    eliminarProducto(element){
+        if(element.name === 'delete'){
+            element.parentElement.parentElement.parentElement.remove();
+        }
     }
 
     mostrarMensaje(){
 
     }
 }
+
+/*******************************************************************/
 
 // EVENTOS
 
@@ -34,7 +56,19 @@ form.addEventListener('submit', (e) =>  {
     const price = document.getElementById('precio').value;
     const date = document.getElementById('fecha').value;
     const amount = document.getElementById('cantidad').value;
-    console.log(name, price, amount, date);
+
+    const product = new Producto(name, price, date, amount); // Nueva Instancia de la clase producto
+    
+    const interfaz = new Interfaz();
+    interfaz.agregarProducto(product);  // A la Nueva Instancia de la clase Interfaz 
+    interfaz.resetForm();               // Llamar al método resetForm();
 
     e.preventDefault();   // Cancelar comportamiento por defecto
 });
+
+const deleteProducts = document.getElementById('lista-productos');
+
+deleteProducts.addEventListener('click', (e) => {
+    const interfaz = new Interfaz();
+    interfaz.eliminarProducto(e.target);
+})
